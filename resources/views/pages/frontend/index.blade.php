f8ed85be842cfe7d4cdf3055090a268c00effb03
arari3s <arlanariandi@gmail.com>
2023-07-08 11:30:48 +0700

    UPDATE: Add Assets Update tentang kami and temukan karir impianmu

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -104,7 +104,7 @@
 
                         <div class="flex items-center mx-auto lg:mx-0 justify-center lg:space-x-4 space-x-2">
                             <a href="https://play.google.com/store/apps/details?id=id.gas.app&amp;amp;hl=en" target="_blank"
-                                class="bg-yellow-400 text-gray-800 inline-flex font-semibold text-base py-4 px-8 rounded-full focus:outline-none">
+                                class="bg-yellow-400 hover:bg-opacity-50 text-gray-800 inline-flex font-semibold text-base py-4 px-8 rounded-full">
                                 Unduh Sekarang
                             </a>
 
@@ -179,9 +179,8 @@
             </div>
         </div>
     </section>
-
     {{-- Popular Fitur --}}
-    <section class="value bg-yellow-50 bg-opacity-5 py-24">
+    <section class="value bg-yellow-500 bg-opacity-5 py-24">
         <div class="max-w-screen-2xl px-4 lg:px-24 mx-auto content" style="font-family: 'Poppins', sans-serif">
             <div class="text-center section-heading mb-20">
                 <div class="mb-3 text-4xl font-bold leading-relaxed text-black lg:text-6xl">Popular Fitur</div>
@@ -632,6 +631,233 @@
             </div>
         </div>
     </section>
+    {{-- Kategori Toko --}}
+    <section class="value py-24" style="background-color: #fafcff">
+        <div class="max-w-screen-2xl px-4 lg:px-24 mx-auto content" style="font-family: 'Poppins', sans-serif">
+            <div class="text-center section-heading mb-20">
+                <div class="mb-3 text-4xl font-bold leading-relaxed text-black lg:text-6xl">Kategori Toko</div>
+                <div class="text-lg font-medium text-gray-400">Apapun jenis tokomu dapat bergabung bersama GAS</div>
+            </div>
+
+            <div class="grid items-center justify-center h-full md:grid-cols-12 gap-4">
+                <div class="h-full md:col-span-4 lg:col-span-3">
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg"
+                                src="https://images.unsplash.com/photo-1534723452862-4c874018d66d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"
+                                alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Toko Kelontong
+                                </h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-full md:col-span-4 lg:col-span-3">
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg"
+                                src="https://images.unsplash.com/photo-1534723452862-4c874018d66d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"
+                                alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Toko Kelontong
+                                </h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-full md:col-span-4 lg:col-span-3">
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg"
+                                src="https://images.unsplash.com/photo-1534723452862-4c874018d66d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"
+                                alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Toko Kelontong
+                                </h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-full md:col-span-4 lg:col-span-3">
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg"
+                                src="https://images.unsplash.com/photo-1534723452862-4c874018d66d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"
+                                alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Toko Kelontong
+                                </h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, placeat!
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- Fitur Maps --}}
+    <section class="value bg-yellow-500 bg-opacity-5 py-24">
+        <div class="max-w-screen-2xl px-4 lg:px-24 mx-auto content" style="font-family: 'Poppins', sans-serif">
+            <div class="text-center section-heading mb-20">
+                <div class="mb-3 text-4xl font-bold leading-relaxed text-black lg:text-6xl">Fitur Maps</div>
+                <div class="text-lg font-medium text-gray-400">Aplikasi GAS akan membantumu dalam memantau stok produk
+                    tokomu dan menjangkau customer yang lebih luas</div>
+            </div>
+
+            <div class="flex items-center justify-center h-full">
+                <img class="rounded-3xl w-3/4"
+                    src="https://developers.google.com/static/maps/images/landing/hero_directions_api_720.png"
+                    alt="" />
+            </div>
+        </div>
+    </section>
+    {{-- Daftar Sekarang --}}
+    <section class="h-full w-full px-8 py-20 bg-white">
+        <div class="flex lg:flex-row flex-col items-center mb-10" style="font-family: 'Poppins', sans-serif">
+            <!-- Left Column -->
+            <div class="w-full lg:w-1/2 text-center justify-center flex lg:mb-0 mb-12">
+                <img src="{{ asset('assets/asset2.png') }}" width="500" alt="" />
+            </div>
+            <!-- Right Column -->
+            <div class="lg:w-1/2 w-full flex flex-col lg:items-start items-center lg:text-left text-center">
+                <h2 class="md:text-5xl text-3xl font-semibold mb-4 tracking-tight">
+                    Daftarkan Tokomu Segera
+                </h2>
+                <h2 class="md:text-5xl text-3xl font-semibold mb-10 tracking-tight">
+                    dan Download Sekarang!
+                </h2>
+                <a href=""
+                    class="bg-yellow-400 hover:bg-opacity-50 px-10 py-4 text-base text-gray-800 font-semibold rounded-xl cursor-pointer focus:outline-none tracking-wide">
+                    Unduh Sekarang
+                </a>
+            </div>
+        </div>
+    </section>
+    {{-- Tentang Kami --}}
+    <section class="h-full w-full border-box bg-white">
+        <div class="mb-8" style="font-family: 'Poppins', sans-serif">
+            <div class="container lg:px-32 md:px-8 sm:px-12 px-5 pt-20 pb-12 mx-auto">
+                <!-- Title Text -->
+                <div class="flex flex-col text-center w-full mb-12">
+                    <h1 class="text-4xl font-semibold title-font mb-2.5 text-gray-800">
+                        Tentang Kami
+                    </h1>
+                    <h2
+                        class="text-base font-light title-font mx-12 lg:w-full md:w-full sm:w-3/6 sm:mx-auto text-gray-800 mb-4">
+                        GAS adalah aplikasi untuk memudahkan pelaku usaha menjangkau calon pembeli lebih luas.
+                        <br class="lg:block hidden" />
+                        Melalui gerakan #AyoBelanjaTokoTetangga
+                        <br class="lg:block hidden" />
+                        GAS mengajak untuk menjelajah dan mendukung usaha-usaha di sekitarmu.
+                    </h2>
+
+                    <div class="flex gap-4 justify-center">
+                        <a href="https://www.facebook.com/aplikasigas" target="_blank">
+                            <svg height="40"
+                                style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+                                version="1.1" viewBox="0 0 512 512" width="100%" xml:space="preserve"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:serif="http://www.serif.com/"
+                                xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g>
+                                    <path
+                                        d="M512,256c0,-141.385 -114.615,-256 -256,-256c-141.385,0 -256,114.615 -256,256c0,127.777 93.616,233.685 216,252.89l0,-178.89l-65,0l0,-74l65,0l0,-56.4c0,-64.16 38.219,-99.6 96.695,-99.6c28.009,0 57.305,5 57.305,5l0,63l-32.281,0c-31.801,0 -41.719,19.733 -41.719,39.978l0,48.022l71,0l-11.35,74l-59.65,0l0,178.89c122.385,-19.205 216,-125.113 216,-252.89Z"
+                                        style="fill:#1877f2;fill-rule:nonzero;" />
+                                    <path
+                                        d="M355.65,330l11.35,-74l-71,0l0,-48.022c0,-20.245 9.917,-39.978 41.719,-39.978l32.281,0l0,-63c0,0 -29.297,-5 -57.305,-5c-58.476,0 -96.695,35.44 -96.695,99.6l0,56.4l-65,0l0,74l65,0l0,178.89c13.033,2.045 26.392,3.11 40,3.11c13.608,0 26.966,-1.065 40,-3.11l0,-178.89l59.65,0Z"
+                                        style="fill:#fff;fill-rule:nonzero;" />
+                                </g>
+                            </svg>
+                        </a>
+
+                        <a href="https://www.instagram.com/aplikasigas/?hl=id" target="_blank">
+                            <svg id="Layer_1" style="enable-background:new 0 0 1000 1000;" version="1.1"
+                                height="40" viewBox="0 0 1000 1000" xml:space="preserve"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <style type="text/css">
+                                    .st0 {
+                                        fill: url(#SVGID_1_);
+                                    }
+
+                                    .st1 {
+                                        fill: #FFFFFF;
+                                    }
+                                </style>
+                                <linearGradient gradientUnits="userSpaceOnUse" id="SVGID_1_" x1="146.4465"
+                                    x2="853.5535" y1="853.5535" y2="146.4465">
+                                    <stop offset="0" style="stop-color:#FFD521" />
+                                    <stop offset="5.510481e-02" style="stop-color:#FFD020" />
+                                    <stop offset="0.1241" style="stop-color:#FEC01E" />
+                                    <stop offset="0.2004" style="stop-color:#FCA71B" />
+                                    <stop offset="0.2821" style="stop-color:#FA8316" />
+                                    <stop offset="0.3681" style="stop-color:#F85510" />
+                                    <stop offset="0.4563" style="stop-color:#F51E09" />
+                                    <stop offset="0.5" style="stop-color:#F30005" />
+                                    <stop offset="0.5035" style="stop-color:#F20007" />
+                                    <stop offset="0.5966" style="stop-color:#E1003B" />
+                                    <stop offset="0.6879" style="stop-color:#D30067" />
+                                    <stop offset="0.7757" style="stop-color:#C70088" />
+                                    <stop offset="0.8589" style="stop-color:#BF00A0" />
+                                    <stop offset="0.9357" style="stop-color:#BB00AF" />
+                                    <stop offset="1" style="stop-color:#B900B4" />
+                                </linearGradient>
+                                <path class="st0"
+                                    d="M500,1000L500,1000C223.9,1000,0,776.1,0,500v0C0,223.9,223.9,0,500,0h0c276.1,0,500,223.9,500,500v0  C1000,776.1,776.1,1000,500,1000z" />
+                                <g>
+                                    <path class="st1"
+                                        d="M500,220.2c91.1,0,101.9,0.3,137.9,2c33.3,1.5,51.4,7.1,63.4,11.8c15.9,6.2,27.3,13.6,39.2,25.5   c11.9,11.9,19.3,23.3,25.5,39.2c4.7,12,10.2,30.1,11.8,63.4c1.6,36,2,46.8,2,137.9s-0.3,101.9-2,137.9   c-1.5,33.3-7.1,51.4-11.8,63.4c-6.2,15.9-13.6,27.3-25.5,39.2c-11.9,11.9-23.3,19.3-39.2,25.5c-12,4.7-30.1,10.2-63.4,11.8   c-36,1.6-46.8,2-137.9,2s-101.9-0.3-137.9-2c-33.3-1.5-51.4-7.1-63.4-11.8c-15.9-6.2-27.3-13.6-39.2-25.5   c-11.9-11.9-19.3-23.3-25.5-39.2c-4.7-12-10.2-30.1-11.8-63.4c-1.6-36-2-46.8-2-137.9s0.3-101.9,2-137.9   c1.5-33.3,7.1-51.4,11.8-63.4c6.2-15.9,13.6-27.3,25.5-39.2c11.9-11.9,23.3-19.3,39.2-25.5c12-4.7,30.1-10.2,63.4-11.8   C398.1,220.5,408.9,220.2,500,220.2 M500,158.7c-92.7,0-104.3,0.4-140.7,2.1c-36.3,1.7-61.1,7.4-82.9,15.9   C254,185.3,234.9,197,216,216c-19,19-30.6,38-39.4,60.5c-8.4,21.7-14.2,46.5-15.9,82.9c-1.7,36.4-2.1,48-2.1,140.7   c0,92.7,0.4,104.3,2.1,140.7c1.7,36.3,7.4,61.1,15.9,82.9C185.3,746,197,765.1,216,784c19,19,38,30.6,60.5,39.4   c21.7,8.4,46.5,14.2,82.9,15.9c36.4,1.7,48,2.1,140.7,2.1s104.3-0.4,140.7-2.1c36.3-1.7,61.1-7.4,82.9-15.9   C746,814.7,765.1,803,784,784c19-19,30.6-38,39.4-60.5c8.4-21.7,14.2-46.5,15.9-82.9c1.7-36.4,2.1-48,2.1-140.7   s-0.4-104.3-2.1-140.7c-1.7-36.3-7.4-61.1-15.9-82.9C814.7,254,803,234.9,784,216c-19-19-38-30.6-60.5-39.4   c-21.7-8.4-46.5-14.2-82.9-15.9C604.3,159.1,592.7,158.7,500,158.7L500,158.7z" />
+                                    <path class="st1"
+                                        d="M500,324.7c-96.8,0-175.3,78.5-175.3,175.3S403.2,675.3,500,675.3S675.3,596.8,675.3,500   S596.8,324.7,500,324.7z M500,613.8c-62.8,0-113.8-50.9-113.8-113.8S437.2,386.2,500,386.2c62.8,0,113.8,50.9,113.8,113.8   S562.8,613.8,500,613.8z" />
+                                    <circle class="st1" cx="682.2" cy="317.8" r="41" />
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="container lg:px-32 md:px-8 sm:px-12 px-5 pt-20 pb-12 mx-auto">
+                <!-- Title Text -->
+                <div class="flex flex-col text-center w-full mb-12">
+                    <h1 class="text-4xl font-semibold title-font mb-2.5 text-gray-800">
+                        Temukan Karir Impianmu
+                    </h1>
+                    <h2
+                        class="text-base font-light title-font mx-12 lg:w-full md:w-full sm:w-3/6 sm:mx-auto text-gray-800 mb-4">
+                        Mari bergabung bersama kami.
+                        <br class="lg:block hidden" />
+                        Jadikan bagian dari kami dan kembangkan bakat, ide kreatifmu dalam GAS.
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
 
 @push('after-style')
